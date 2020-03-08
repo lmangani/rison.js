c509c42c70de7ba0d91efec3764f9050a83e0f6b
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2020-03-08 17:55:56 +0100

    update package, extend test

--- test/index.js
+++ test/index.js
@@ -3,17 +3,22 @@ var should = require('chai').should(),
     encode = rison.encode,
     decode = rison.decode;
 
+var buffer;
+
 describe('#escape', function() {
   it('RISON Encoder', function() {
-    var testme = { "ami" : "correct" };
-    encode(testme).should.equal('(ami:correct)');
+    var testme = { "ami" : "correct", "array": ["one"] };
+    buffer = encode(testme)
+    console.log(buffer);
+    encode(testme).should.equal('(ami:correct,array:!(one))');
   });
 
 });
 
 describe('#unescape', function() {
   it('RISON Decoder', function() {
-    var testme = decode('(ami:correct)');
+    var testme = decode(buffer);
+    console.log(testme);
     testme.ami.should.equal('correct');
   });
 
